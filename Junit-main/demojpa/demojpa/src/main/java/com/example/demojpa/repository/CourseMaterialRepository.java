ac65c9400ab0aa486f54f907611c7b9c0512fd05
Tarun Teja Kukkala <75311096+Lucifer732@users.noreply.github.com>
2022-08-03 20:59:26 +0530

    Delete Junit-main/demojpa/demojpa directory

--- Junit-main/demojpa/demojpa/src/main/java/com/example/demojpa/repository/CourseMaterialRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demojpa.repository;
-
-import com.example.demojpa.Entity.CourseMaterial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseMaterialRepository extends JpaRepository<CourseMaterial,Integer> {
-}
